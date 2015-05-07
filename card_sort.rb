0c6a084be95cef1ba3934ca4fa3be830d9d048eb
Benjamin Rahn <brahn@stripe.com>
2015-05-06 21:11:53 -1000

    working card sort

---
+++ card_sort.rb
@@ -0,0 +1,44 @@
+class CardSort
+
+  def initialize(deck_size)
+    @deck_size = deck_size
+    @steps = [random_permutation(deck_size)]
+  end
+
+  def sort
+    (0..(num_steps - 1)).each do |step_num|
+      do_step(step_num)
+    end
+    @steps
+  end
+
+  def num_steps
+    Math.log(@deck_size, 2).ceil
+  end
+
+  def do_step(step_num)
+    @steps << sort_by_bit(@steps.last, step_num)
+  end
+
+  def random_permutation(deck_size)
+    (0..(deck_size - 1)).sort_by{rand()}
+  end
+
+
+  def sort_by_bit(list, bit_num)
+    pile0 = []
+    pile1 = []
+    list.each do |elt|
+      if bit(elt, bit_num) == 0
+        pile0 << elt
+      else
+        pile1 << elt
+      end
+    end
+    pile0 + pile1
+  end
+
+  def bit(num, bit_num)
+    (num / (2 ** bit_num)) % 2
+  end
+end
